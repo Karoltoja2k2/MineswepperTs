e9d4a0bf3d920722575fbb02d11ff9a397520ce5
Karoltoja2k2 <karol.zedzian1999@gmail.com>
2020-05-02 20:19:33 +0200

    bomb animation, settings btn position change

--- src/components/game.ts
+++ src/components/game.ts
@@ -1,39 +1,35 @@
 const Game = () => {
     var template =
     `
-    <div class="background">
+    <div class="background" id="background">
         <div class="gameContainer" id="gameContainer">
             <div class="menu">
                 <div class="counter">
                     <h1 id="bombCounter">091</h1>
                 </div>
-                <div>
-                    <button class="uibtn" id="newGameBtn"> 
-                        <span class="material-icons" style="font-size:36px;">
-                            fiber_new
-                        </span>
-                    </button> 
-                    <button class="uibtn" id="settingBtn">
-                        <span class="material-icons" style="font-size:36px;">
-                            settings
-                        </span>
-                    </button>
-                </div>
+                <button class="newgamebtn" id="newGameBtn"> 
+                    <span class="material-icons" style="font-size:36px;">
+                        fiber_new
+                    </span>
+                </button>                     
                 <div class="counter">
                     <h1 id="timeCounter">012</h1>
                 </div>
             </div>
             
             <div class="grid" id="grid">
-            </div>
-            
-        </div>  
+            </div>            
+        </div> 
+        
+        <button class="settingsbtn" id="settingsBtn">
+            <span class="material-icons" style="font-size:36px;">
+                settings
+            </span>
+        </button>
 
         <div class="dropdown-content" id="dropdown-content">
             <div class="bg">
             </div>
-
-
             <h1>Mineswepper</h1>
             <button id="changeSizeBtn">Change size</button>
             <input type="range" min="10" max="50" value="30" class="slider rangeInput" id="squareSizeSlider">
